673149d1c07523c82fed16baec4b0e2228986f75
wellington-bonjardim <wellingtonbonjardim7@gmail.com>
2023-05-19 16:24:29 -0300

    Resolvendo o problema de distância entre o texto e os ícones e o problema de sobreposição das classes

--- src/componentes/Footer/index.tsx
+++ src/componentes/Footer/index.tsx
@@ -5,17 +5,17 @@ import { faTwitter, faInstagram, faFacebook } from '@fortawesome/free-brands-svg
 export default function Footer() {
   return (
     <footer>
-      <p>Todos os direitos são reservados ao <strong>Botafogo de Futebol e Regatas</strong></p>
+      <p className={styles.paragrafo}>Todos os direitos são reservados ao <strong>Botafogo de Futebol e Regatas</strong></p>
         <div className={styles.social}>
-          <a href="https://twitter.com/Botafogo?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" target="_blank" rel="noreferrer">
+          <a href="https://twitter.com/Botafogo?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" target="_blank" rel="noreferrer" className={styles.linkSocial}>
             <FontAwesomeIcon icon={faTwitter} size="2x" inverse className={styles.hover} />
           </a>
 
-          <a href="https://www.instagram.com/botafogo/?hl=pt" target="_blank" rel="noreferrer">
+          <a href="https://www.instagram.com/botafogo/?hl=pt" target="_blank" rel="noreferrer" className={styles.linkSocial}>
             <FontAwesomeIcon icon={faInstagram} size="2x" inverse className={styles.hover}/>
           </a>
 
-          <a href="https://www.facebook.com/Botafogo/" target="_blank" rel="noreferrer">
+          <a href="https://www.facebook.com/Botafogo/" target="_blank" rel="noreferrer" className={styles.linkSocial}>
             <FontAwesomeIcon icon={faFacebook} size="2x" inverse className={styles.hover} />
           </a>
         </div>
--- src/componentes/PaginaPadrao/index.tsx
+++ src/componentes/PaginaPadrao/index.tsx
@@ -1,4 +1,4 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import styles from './PaginaPadrao.module.css';
 import Logo from './logo_camisa7.png';
 import MenuLink from 'componentes/MenuLink';
@@ -8,7 +8,7 @@ export default function PaginaPadrao() {
   return (
     <>
       <header className={styles.header}>
-        <img src={Logo} alt="Imagem torcida Botafogo"  className={styles.logo}/>
+        <img src={Logo} alt="Imagem torcida Botafogo" className={styles.logo}/>
         <nav>
           <MenuLink to="/">Início</MenuLink>
           <MenuLink to="/beneficios">Benefícios</MenuLink>
